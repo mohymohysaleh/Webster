6dc8d6551e8a414868477689357a68389c442e0e
Kareem Adel <kareem.adel20047@gmail.com>
2025-04-09 01:31:10 +0200

    added home page

---
+++ client/src/pages/home/PlaylistsPage.jsx
@@ -0,0 +1,12 @@
+export default function PlaylistsPage() {
+    return (
+      <div className="p-4">
+        <h1 className="h3 mb-4">Your Playlists</h1>
+        <div className="text-center text-secondary mt-5">
+          <p>You don't have any playlists yet</p>
+          <button className="btn btn-outline-light mt-3">Create Playlist</button>
+        </div>
+      </div>
+    )
+  }
+  
