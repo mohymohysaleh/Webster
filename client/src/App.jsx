737b79bf03356e017f1a4adec96049ffb8a552a2
Kareem Adel <kareem.adel20047@gmail.com>
2025-05-12 04:01:31 +0300

    update app.jsx again

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -57,151 +57,34 @@ function AppRoutes() {
   const { user } = useAuth();
 
   return (
-    <>
-      <Routes>
-        <Route path="/auth/google/callback" element={<GoogleCallback />} />
-        {!user ? (
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="*" element={<Navigate to="/login" />} />
-          </Routes>
-        ) : (
-          <div className="d-flex vh-100 music-app">
-            <Sidebar />
-            <div className="main-content">
-              <SettingsButton />
-              <Routes>
-                {/* Admin-only route */}
-                <Route
-                  path="/admin"
-                  element={
-                    <PrivateRoute adminOnly>
-                      <AdminPage />
-                    </PrivateRoute>
-                  }
-                />
-                
-                {/* Regular user routes */}
-                <Route
-                  path="/"
-                  element={
-                    <PrivateRoute>
-                      <HomePage />
-                    </PrivateRoute>
-                  }
-                />
-                <Route
-                  path="/search"
-                  element={
-                    <PrivateRoute>
-                      <SearchPage />
-                    </PrivateRoute>
-                  }
-                />
-                <Route
-                  path="/playlists"
-                  element={
-                    <PrivateRoute>
-                      <PlaylistsPage />
-                    </PrivateRoute>
-                  }
-                />
-                <Route
-                  path="/playlist/:id"
-                  element={
-                    <PrivateRoute>
-                      <PlaylistDetailPage />
-                    </PrivateRoute>
-                  }
-                />
-                <Route
-                  path="/likes"
-                  element={
-                    <PrivateRoute>
-                      <LikesPage />
-                    </PrivateRoute>
-                  }
-                />
-                <Route
-                  path="/create-playlist"
-                  element={
-                    <PrivateRoute>
-                      <CreatePlaylistPage />
-                    </PrivateRoute>
-                  }
-                />
+    <Routes>
+      {/* Always available */}
+      <Route path="/auth/google/callback" element={<GoogleCallback />} />
 
-                <Route
-                path="/live-stream"
-                 element={
-                     <PrivateRoute>
-                     <LiveStreamPage />
-                     </PrivateRoute>
-                          }/>
-                <Route
-                  path="/settings"
-                  element={
-                    <PrivateRoute>
-                      <SettingsPage />
-                    </PrivateRoute>
-                  }
-                />
-                <Route
-                  path="/account-settings"
-                  element={
-                    <PrivateRoute>
-                      <AccountSettingsPage />
-                    </PrivateRoute>
-                  }
-                />
-                <Route
-                  path="/genre/:id"
-                  element={
-                    <PrivateRoute>
-                      <GenreDetailPage />
-                    </PrivateRoute>
-                  }
-                />
-                
-                {/* Add Streamer and Listener routes */}
-                <Route
-                  path="/streamer"
-                  element={
-                    <PrivateRoute>
-                      <Streamer />
-                    </PrivateRoute>
-                  }
-                />
-                <Route
-                  path="/listener"
-                  element={
-                    <PrivateRoute>
-                      <Listener />
-                    </PrivateRoute>
-                  }
-                />
-                
-                {/* Catch-all route */}
-                <Route path="*" element={<Navigate to="/" />} />
-              </Routes>
-            </div>
-            <MusicPlayer />
-          </div>
-        )}
-      </Routes>
-      <ToastContainer 
-        position="bottom-right"
-        autoClose={3000}
-        hideProgressBar={false}
-        newestOnTop
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="dark"
-      />
-    </>
+      {!user ? (
+        <>
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<Navigate to="/login" />} />
+        </>
+      ) : (
+        <>
+          <Route path="/" element={<PrivateRoute><HomePage /></PrivateRoute>} />
+          <Route path="/search" element={<PrivateRoute><SearchPage /></PrivateRoute>} />
+          <Route path="/playlists" element={<PrivateRoute><PlaylistsPage /></PrivateRoute>} />
+          <Route path="/playlist/:id" element={<PrivateRoute><PlaylistDetailPage /></PrivateRoute>} />
+          <Route path="/likes" element={<PrivateRoute><LikesPage /></PrivateRoute>} />
+          <Route path="/create-playlist" element={<PrivateRoute><CreatePlaylistPage /></PrivateRoute>} />
+          <Route path="/live-stream" element={<PrivateRoute><LiveStreamPage /></PrivateRoute>} />
+          <Route path="/settings" element={<PrivateRoute><SettingsPage /></PrivateRoute>} />
+          <Route path="/account-settings" element={<PrivateRoute><AccountSettingsPage /></PrivateRoute>} />
+          <Route path="/genre/:id" element={<PrivateRoute><GenreDetailPage /></PrivateRoute>} />
+          <Route path="/admin" element={<PrivateRoute adminOnly><AdminPage /></PrivateRoute>} />
+          <Route path="/streamer" element={<PrivateRoute><Streamer /></PrivateRoute>} />
+          <Route path="/listener" element={<PrivateRoute><Listener /></PrivateRoute>} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </>
+      )}
+    </Routes>
   );
 }
 
